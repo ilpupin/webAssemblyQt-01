0d69ad1560b3881ee0237d4bdae1cd3d9aae0431
ilpupin <105357860+ilpupin@users.noreply.github.com>
2023-08-11 15:25:16 +0200

    Add files via upload

---
+++ apptest01_qmltyperegistrations.cpp
@@ -0,0 +1,22 @@
+/****************************************************************************
+** Generated QML type registration code
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <QtQml/qqml.h>
+#include <QtQml/qqmlmoduleregistration.h>
+
+
+
+#if !defined(QT_STATIC)
+#define Q_QMLTYPE_EXPORT Q_DECL_EXPORT
+#else
+#define Q_QMLTYPE_EXPORT
+#endif
+Q_QMLTYPE_EXPORT void qml_register_types_test01()
+{
+    qmlRegisterModule("test01", 1, 0);
+}
+
+static const QQmlModuleRegistration registration("test01", qml_register_types_test01);
